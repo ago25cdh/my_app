c9bd0a2b3ae21c572109aa957b6b03291dcc6c65
chadongho5525 <ago25cdh@icloud.com>
2018-10-09 13:17:15 +0900

    start

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// import module
 var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+// connect db
 mongoose.connect(process.env.MONGO_PATH, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.once("open", function(){
@@ -12,78 +15,11 @@ db.on("error", function(err){
   console.log("db error : ", err);
 });
 
-var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
-});
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name:"myData"},function(err,data){
-  if(err) return console.log("data error",err);
-  if(!data){
-    Data.create({name:"myData",count:0},function(err,data){
-      if(err) return console.log("data error",err);
-      console.log("counter initialized :",data);
-    });
-  }
-});
+// model setting
 
-
+//view setting
 app.set("view engine", 'ejs');
 
-app.use(express.static(path.join(__dirname , '/public')));
-
-
-app.get('/', function(req,res){
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("data error: ",err);
-    data.count++;
-    data.save(function(err){
-      if(err) return console.log("data error: ", err);
-      res.render('my_first_ejs',data);
-    });
-  });
-});
-
-app.get('/reset', function(req,res){
-  setCounter(res,0);
-});
-
-app.get('/set/count', function(req,res){
-  if(req.query.count) setCounter(res, req.query.count);
-  else getCounter(res);
-});
-
-app.get('/set/:num', function(req,res){
-  if(req.params.num) setCounter(res,req.params.num);
-  else getCounter(res);
-});
-
-function setCounter(res, num){
-  console.log("setCounter");
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("data error",err);
-    data.count = num;
-    data.save(function(err){
-      if(err) return console.log("data error",err);
-      res.render('my_first_ejs',data);
-    });
-  });
-}
-function getCounter(res){
-  console.log("getCounter");
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("data error",err);
-    res.render('my_first_ejs',data);
-  });
-}
-
-
-
-
-app.get('/', function(req, res){
-  res.render('my_first_ejs');
-});
-
-app.listen(3000, function(){
-  console.log('server on !');
-});
+// set middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
